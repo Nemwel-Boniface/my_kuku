b8ae5ef2e6a75e17f073dceec788ecd4718ec1e3
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-04-12 21:24:03 +0300

    Update inclucluded:   - Update to the project schema file by running all pending migrations   - Linked the navbar abd footer navlinks for contacts us page

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_191643) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_181158) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "abouts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

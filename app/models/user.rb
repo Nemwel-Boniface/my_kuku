c49b69aff04416fcf554ff2b44f75716d40c503f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-04-18 19:27:16 +0300

    Update included:   - Added the has_one_attached associations for the profile_photo to the users model   - Added the file upload in the signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  has_one_attached :profile_photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

690bc8009c044f9dcd54ccf406ed8af4d7be8de3
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-05-13 22:03:11 +0300

    Update instruct the friendly_id gem to use the blog_title attribute of the model as the basis for generating the slug,

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,17 @@
 class Blog < ApplicationRecord
+  # Friendly for the custom slug
+  extend FriendlyId
+  friendly_id :blog_title, use: :slugged
+
   belongs_to :user
   has_one_attached :blog_image
   after_save :update_blogs_counter
 
   def update_blogs_counter
     user.increment!(:blogs_counter)
+  end
+
+  def to_param
+    slug
   end
 end

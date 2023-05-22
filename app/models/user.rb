73470f844ee2c913702a23e165443ec43abf6dea
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-05-22 09:11:58 +0300

    Update added validations to the user model's attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  # validations
+  validates :names, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :email, presence: true
+  validates :profile_photo, presence: true
+  validates :role, presence: true
+  validates :blogs_counter, presence: true, numericality: { only_integer: true }
+  validates :mobile_number, presence: true, length: { is: 9 }, numericality: { only_integer: true }
+  validates :status, presence: true
+  validates :location, presence: true
+
+  # associations
+
   has_one_attached :profile_photo
   has_many :blogs
   # Include default devise modules. Others available are:

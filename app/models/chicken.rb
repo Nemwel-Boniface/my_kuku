cb2c1cc9bbdbd77434ccca71c4c7bce1212b2167
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-06-13 23:02:45 +0300

    Update created a custom method for generating a tag number based on the month,date and id of the record

--- app/models/chicken.rb
+++ app/models/chicken.rb
@@ -1,2 +1,19 @@
 class Chicken < ApplicationRecord
+  before_create :generate_tag_number
+
+  private
+
+  def generate_tag_number
+    # Get the corrent month eh June
+    current_month = Date.today.strftime('%B')
+
+    # Get the current date in the month
+    current_date = Date.today.strftime('%d')
+
+    # Let's append a few zeros before the ID of the record being created
+    tag_number_with_zeros = format('%03d', id)
+
+    # Now let's wire them all up together
+    self.tag_number = "#{current_month}#{current_date}#{tag_number_with_zeros}"
+  end
 end

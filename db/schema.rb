183dd10825c4ea2e0625bee4a058f81e5e07c8e9
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2023-06-16 22:31:04 +0300

    Update included:   - generated a migration that adds the slug column to the Chickens table   - Run migrations to update the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_061741) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_193021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_061741) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_chickens_on_slug", unique: true
     t.index ["user_id"], name: "index_chickens_on_user_id"
   end
 

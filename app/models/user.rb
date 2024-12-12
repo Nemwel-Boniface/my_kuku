e080bb6b050e6a7ac00fbc06ddb7695c8b70597f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-12-12 11:10:17 +0300

    Update included:   - Dynamically assign users roles during account creation   - IMplement authorization ensuring that different set of users get to see different accesseg user should not create an egg record

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,26 @@ class User < ApplicationRecord
   validates :location, presence: true
 
   # associations
-
   has_one_attached :profile_photo
   has_many :blogs
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Callbacks
+  before_create :assign_role
+
+  private
+
+  # Assign role dynamically based on the count of existing users
+  def assign_role
+    user_count = User.count
+    self.role = case user_count
+                when 0 then "admin"
+                when 1 then "worker"
+                else "user"
+                end
+  end
 end

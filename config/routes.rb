57320dc80b4a9f5eef41fc250b0a13f3a40de8ee
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-12-12 10:50:59 +0300

    Updaye included:   - Created the invoices page that allows a user to see the items they selected to buy   - updated the routes to include the routes for creating the invoice

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
       get :buy
     end
   end
+  post 'sales/confirm', to: 'sales#confirm', as: 'confirm_sale'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
